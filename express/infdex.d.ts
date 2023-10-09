b38336652238e3494fea6d7441b05f61f9761965
Samuel <sam404kim@gmail.com>
2023-10-09 11:24:10 -0700

    added types for packages

--- express/infdex.d.ts
+++ express/infdex.d.ts
@@ -1,8 +1,11 @@
 
 declare global {
   namespace Express {
+    express: any
     interface Request {
-      session?: Record<string,any>
+      session?: Record<string,any>,
+      'body-parser' ?: any,
+      express: any
     }
   }
 }
