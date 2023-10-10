19a327a1b0ff5d437a5a4732cdf6fe95dd90c27c
Samuel <sam404kim@gmail.com>
2023-10-09 17:04:56 -0700

    added dynamic port

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -4,7 +4,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:19006',
   'http://192.168.0.13:19000',
-  'https://65224a7a94794e0008bec647--venerable-begonia-88eaeb.netlify.app',
+  'https://652264354afda60008b360af--venerable-begonia-88eaeb.netlify.app',
   '65224a7a94794e0008bec647--venerable-begonia-88eaeb.netlify.app',
 ];
 
