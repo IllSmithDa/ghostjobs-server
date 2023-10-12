33fd923032cdd3b17efd9602f31c5eae7ec53b82
Samuel <sam404kim@gmail.com>
2023-10-11 18:38:54 -0700

    url updated

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -3,9 +3,9 @@ const allowedOrigins = [
   //'https://652264354afda60008b360af--venerable-begonia-88eaeb.netlify.app',
   //'65224a7a94794e0008bec647--venerable-begonia-88eaeb.netlify.app',
   //'https://venerable-begonia-88eaeb.netlify.app',
-  'https://ghostedon.com',
-  'https://ghostjobs-client-cyhb1lyxf-illsmithdas-projects.vercel.app',
-  'https://ghostjobs-client.vercel.app',
+  // 'https://ghostedon.com',
+  // 'https://ghostjobs-client-cyhb1lyxf-illsmithdas-projects.vercel.app',
+  // 'https://ghostjobs-client.vercel.app',
   // 'https://ghostjobs-client-git-main-illsmithdas-projects.vercel.app'
 ];
 
