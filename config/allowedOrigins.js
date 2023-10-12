c9097b4c09ef36eac4905a0e7c264703235d202d
Samuel <sam404kim@gmail.com>
2023-10-11 19:46:57 -0700

    added another origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   '65224a7a94794e0008bec647--venerable-begonia-88eaeb.netlify.app',
   'https://venerable-begonia-88eaeb.netlify.app',
   'https://ghostedon.com',
+  'https://www.ghostedon.com',
   'https://ghostjobs-client-cyhb1lyxf-illsmithdas-projects.vercel.app',
   'https://ghostjobs-client.vercel.app',
   'https://ghostjobs-client-git-main-illsmithdas-projects.vercel.app'
