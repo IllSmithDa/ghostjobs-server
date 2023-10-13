0bcc26fa154f99ce577b0130ee7a9a2a88e2c48a
Samuel <sam404kim@gmail.com>
2023-10-13 09:31:07 -0700

    testing additional req headers

--- index.js
+++ index.js
@@ -96,18 +96,19 @@ app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// app.all('*', (req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (originList.indexOf(origin) !== -1 || !origin) {
-//     res.header("Access-Control-Allow-Origin", 'https://www.ghostedon.com');
-//   }
-//   res.header("Cache-Control", "no-store,no-cache,must-revalidate");
-//   res.header("Access-Control-Allow-Headers", ["Content-Type","X-Requested-With","X-HTTP-Method-Override","Accept"]);
-//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Vary", "Origin");
-//   next();
-// })
+app.all('*', (req, res, next) => {
+  // const origin = req.headers.origin;
+  // if (originList.indexOf(origin) !== -1 || !origin) {
+  //   res.header("Access-Control-Allow-Origin", 'https://www.ghostedon.com');
+  // }
+  res.set('credentials', 'include');
+  res.header("Cache-Control", "no-store,no-cache,must-revalidate");
+  res.header("Access-Control-Allow-Headers", ["Content-Type","X-Requested-With","X-HTTP-Method-Override","Accept"]);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Vary", "Origin");
+  next();
+})
 
 app.get('/auth/google', 
   passport.authenticate('google', {
